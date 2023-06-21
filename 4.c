2c56da90c2ea4f6dd16aad1b4a1f67ce48b6b638
l3aShn9l <97799716+l3aShn9l@users.noreply.github.com>
2023-06-21 05:41:29 +0300

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,55 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+const int BUFF_SIZE = 1024;
+
+int main(int argc, char **argv) {
+int fd;
+int fdout;
+char buffer[BUFF_SIZE];
+ssize_t read_bytes;
+ssize_t written_bytes;
+
+if (argc < 3) {
+fprintf(stderr, "Too few arguments\n");
+exit(1);
+}
+
+fd = open(argv[1], O_RDONLY);
+if (fd < 0) {
+fprintf(stderr, "Can't open file\n");
+exit(1);
+}
+
+fdout = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IXUSR);
+if (fdout < 0) {
+fprintf(stderr, "Can't create file\n");
+close(fd);
+exit(1);
+}
+
+while ((read_bytes = read(fd, buffer, BUFF_SIZE)) > 0) {
+written_bytes = write(fdout, buffer, read_bytes);
+if (written_bytes != read_bytes) {
+fprintf(stderr, "Can't write to file\n");
+close(fd);
+close(fdout);
+exit(1);
+}
+}
+
+if (read_bytes < 0) {
+fprintf(stderr, "Can't read file\n");
+close(fd);
+close(fdout);
+exit(1);
+}
+
+close(fd);
+close(fdout);
+return 0;
+}
